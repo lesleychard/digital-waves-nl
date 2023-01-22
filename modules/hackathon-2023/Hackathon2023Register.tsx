448687f5ced3c8abaf419f18068d8c10d55c76b9
Freddie-Pike <freddiepike709@gmail.com>
2023-01-22 17:09:13 -0330

    Fix type errors in Hackathon2023Register pls

--- modules/hackathon-2023/Hackathon2023Register.tsx
+++ modules/hackathon-2023/Hackathon2023Register.tsx
@@ -1,17 +1,17 @@
-import { ReactElement, useState } from 'react';
-import { useQuery, useMutation } from '@tanstack/react-query';
+import { ReactElement, useState, SyntheticEvent } from 'react';
+import { useMutation } from '@tanstack/react-query';
 import { makeStyles } from '@material-ui/core/styles';
 import { container } from '../../styles/helpers/extend';
-import md5 from "md5";
-import RegistrationForm from '../../components/RegistrationForm';
+// import md5 from "md5";
+// import RegistrationForm from '../../components/RegistrationForm';
 
-const CONSENT_LABEL = (
-  'I give my consent to Digital Waves to contact the above student '
-  + 'with information about the 2023 Hackathon.'
-);
+// const CONSENT_LABEL = (
+//   'I give my consent to Digital Waves to contact the above student '
+//   + 'with information about the 2023 Hackathon.'
+// );
 
-const PARENT_MERGE_TAG = 'PARENT_23';
-const PARTICIPANT_MERGE_TAG = 'PARTIC_23';
+// const PARENT_MERGE_TAG = 'PARENT_23';
+// const PARTICIPANT_MERGE_TAG = 'PARTIC_23';
 
 const useStyles = makeStyles(
   (theme) => ({
@@ -36,14 +36,19 @@ const useStyles = makeStyles(
   })
 );
 
+type MutationTypes = {
+  email?: string;
+  parentEmail?: string;
+};
+
 const Hackathon2023Register = (): ReactElement => {
   const classes = useStyles();
 
-  const [subscriberEmail, setSubscriberEmail] = useState();
-  const [subscriberParentEmail, setSubscriberParentEmail] = useState();
+  const [subscriberEmail, setSubscriberEmail] = useState<string | undefined>();
+  const [subscriberParentEmail, setSubscriberParentEmail] = useState<string | undefined>();
 
   const mutation = useMutation({
-    mutationFn: (newFormData) => {
+    mutationFn: (newFormData: MutationTypes) => {
       return fetch('/api/subscribe', {
         method: 'POST',
         headers: {
@@ -54,7 +59,7 @@ const Hackathon2023Register = (): ReactElement => {
     },
   });
 
-  const handleSubmit = (event: Event): void => {
+  const handleSubmit = (event: SyntheticEvent): void => {
     event.preventDefault();
     mutation.mutate({ email: subscriberEmail, parentEmail: subscriberParentEmail });
     console.log('submit');
@@ -70,8 +75,9 @@ const Hackathon2023Register = (): ReactElement => {
               type="email"
               id="email"
               value={subscriberEmail}
-              onChange={(event) => {
-                setSubscriberEmail(event.target.value);
+              onChange={(event: React.FormEvent<HTMLInputElement>) => {
+                const element = event.currentTarget as HTMLInputElement;
+                setSubscriberEmail(element.value);
               }}
             />
           </label>
@@ -81,8 +87,9 @@ const Hackathon2023Register = (): ReactElement => {
               type="parent_email"
               id="parent_email"
               value={subscriberParentEmail}
-              onChange={(event) => {
-                setSubscriberParentEmail(event.target.value);
+              onChange={(event: React.FormEvent<HTMLInputElement>) => {
+                const element = event.currentTarget as HTMLInputElement;
+                setSubscriberParentEmail(element.value);
               }}
             />
           </label>
@@ -98,12 +105,12 @@ const Hackathon2023Register = (): ReactElement => {
     </div>
 
 
-    // <RegistrationForm
-    //   title={"ABC"}
-    //   consentLabel={CONSENT_LABEL}
-    //   parentMergeTag={PARENT_MERGE_TAG}
-    //   participantMergeTag={PARTICIPANT_MERGE_TAG}
-    // />
+  // <RegistrationForm
+  //   title={"ABC"}
+  //   consentLabel={CONSENT_LABEL}
+  //   parentMergeTag={PARENT_MERGE_TAG}
+  //   participantMergeTag={PARTICIPANT_MERGE_TAG}
+  // />
   );
 };
 
