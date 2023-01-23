faff98cb35e55a8b3cb4ce72ae45ce4e80df8e6d
Lesley Chard <lac275@mun.ca>
2023-01-22 20:45:04 -0330

    Finishing touches on program info section styles

--- modules/hackathon-2023/Hackathon2023ProgramInfo.tsx
+++ modules/hackathon-2023/Hackathon2023ProgramInfo.tsx
@@ -8,33 +8,33 @@ import Button from '../../components/Button';
 
 const EVENTS = [
   {
-   title: 'Virtual Beginners Workshops',
-   image: '',
-   label: 'Learn To Design & Code',
-   description: 'Learn the digital skills needed for your hackathon project, no prior design or coding knowledge required. We’ll teach you everything you need to know!',
-   time: '12pm - 2pm (Online)',
-   date: 'April 1 & 8, 2023', 
-   step: '1'
+    title: 'Virtual Beginners Workshops',
+    image: 'assets/images/emojis/emoji-teacher.svg',
+    label: 'Learn To Design & Code',
+    description: 'Learn the digital skills needed for your hackathon project, no prior design or coding knowledge required. We’ll teach you everything you need to know!',
+    time: '12pm - 2pm (Online)',
+    date: 'April 1 & 8, 2023', 
+    step: '1',
   },
   {
     title: 'Work with Industry Mentors',
-    image: '',
+    image: 'assets/images/emojis/emoji-tech-girl.svg',
     label: 'Get 1:1 Support',
     description: 'Level-up your hackathon project with the help of our diverse pool of local design and coding experts in NL’s technology sector.',
     time: '12pm - 2pm (Online)',
     date: 'April 15 & 22, 2023',
-    step: '2'
+    step: '2',
   },
   {
-   title: 'In-Person Or Online Event',
-   image: '',
-   label: 'Show Off Your New Skills',
-   description: 'Join us this spring for a virtual-friendly, in-person event to celebrate the launch of InQueeries, where your work will be exhibited!',
-   time: '1pm - 4pm (Online or In-Person)',
-   date: 'May 6, 2023',
-   step: '3'
+    title: 'In-Person Or Online Event',
+    image: 'assets/images/emojis/emoji-rockstar.svg',
+    label: 'Show Off Your New Skills',
+    description: 'Join us this spring for a virtual-friendly, in-person event to celebrate the launch of InQueeries, where your work will be exhibited!',
+    time: '1pm - 4pm (Online or In-Person)',
+    date: 'May 6, 2023',
+    step: '3',
   },
-]
+];
 
 const useStyles = makeStyles(
   (theme) => {
@@ -43,9 +43,7 @@ const useStyles = makeStyles(
       root: {
         background: 'url(assets/images/brand/section-bg-teal.jpg) no-repeat center center',
         backgroundSize: 'cover',
-        [theme.breakpoints.up('md')]: {
-          textAlign: 'center',
-        },
+        textAlign: 'center',
       },
       container: {
         ...containerSm(theme),
@@ -53,6 +51,7 @@ const useStyles = makeStyles(
       typographyOverline: {
         marginBottom: theme.spacing(2),
         color: 'white',
+        fontWeight: theme.typography.fontWeightBold,
         [theme.breakpoints.up('md')]: {
           marginBottom: theme.spacing(6),
         },
@@ -60,76 +59,92 @@ const useStyles = makeStyles(
       typographyH1: {
         color: light,
         marginBottom: theme.spacing(2),
+        '& strong': {
+          color: theme.palette.text.primary,
+        },
       },
       gridContainer: {
-        marginTop: theme.spacing(8),
-        [theme.breakpoints.down('xs')]: {
-          padding: theme.spacing(4),
-        },
+        margin: `${theme.spacing(8)}px 0`,
+        width: '100%',
       },
       gridItem: {
         textAlign: 'center',
-        marginBottom: theme.spacing(8),
         position: 'relative',
 
       },
       stepCircle: {
         position: 'absolute',
-        backgroundColor: '#9E99DC',
-        color: 'white',
-        
+        background: theme.palette.lowlight.dark,
+        color: theme.palette.lowlight.contrastText,
+        left: '-1rem',
+        top: '2.5rem',
+        zIndex: 2,
+        width: '2.5rem',
+        height: '2.5rem',
+        borderRadius: '50%',
+        fontWeight: theme.typography.fontWeightBold,
+        fontSize: '1.2rem',
+        lineHeight: '2.7rem',
       },
       title: {
-        fontFamily: fontFamilyHeading,
-        fontSize: '1.25rem',
-        lineHeight: 1.2,
-        marginBottom: '1rem'
+        fontFamily: fontFamilyBody,
+        textTransform: 'uppercase',
+        fontWeight: theme.typography.fontWeightBold,
+        fontSize: '0.8rem',
+        lineHeight: 1.5,
       },
       label: {
         fontFamily: fontFamilyHeading,
-        fontSize: '24px',
+        fontSize: '1.5rem',
         display: 'block',
         lineHeight: 1,
-        marginBottom: '1rem'
+        marginTop: theme.spacing(1),
+        marginBottom: theme.spacing(3),
       },
       date: {
         fontFamily: fontFamilyBody,
-        fontSize: '1.15rem',
+        fontSize: '1rem',
+        fontWeight: theme.typography.fontWeightBold,
       },
       description: {
         fontFamily: fontFamilyBody,
         fontSize: '1rem',
-        marginBottom: '1rem'
+        marginBottom: theme.spacing(2),
       },
       image: {
-
+        marginBottom: theme.spacing(2),
+        '& > img': {
+          width: '5rem',
+        },
       },
       time: {
         fontFamily: fontFamilyBody,
-        fontSize: '1.15rem',
+        fontSize: '1rem',
+        fontWeight: theme.typography.fontWeightBold,
       },
       containerRetroUI: {
-        maxWidth: retroUIWidth,
-        margin: '0 auto',
+        width: '100%',
+        height: '100%',
+        padding: `${theme.spacing(2)}px 0`,
         position: 'relative',
       },
       retroUI: {
         position: 'relative',
-        width: '20rem',
-        height: '20rem',
+        height: '100%',
         zIndex: 1,
-        marginBottom: theme.spacing(2),
       },
-      button: {
-
+      containerCTAs: {
+        width: '100%',
+        zIndex: 1,
+        position: 'relative',
       },
-      faq: {
-        color: 'black',
-        margin: '1rem'
-      }
-    }
+      buttonFaq: {
+        marginTop: theme.spacing(2),
+        textDecoration: 'underline',
+      },
+    };
   }
-)
+);
 
 const Hackathon2023ProgramInfo = (): ReactElement => {
   const classes = useStyles();
@@ -144,8 +159,10 @@ const Hackathon2023ProgramInfo = (): ReactElement => {
         >
           Program Information & Key Dates
         </Typography>
-        <Typography variant="h1" className={classes.typographyOverline}>
-          What’s included in the <span style={{color: 'black'}}>2023 Hackathon</span>?
+        <Typography variant="h1" className={classes.typographyH1}>
+          What&rsquo;s included in the&nbsp;
+          <strong>2023 Hackathon</strong>
+          ?
         </Typography>
         <Typography>
         Interested in participating in our 2023 Hackathon? Registration is free, and participants will be entered to win prizes and swag from our amazing local sponsors. Here’s what to expect by signing up:
@@ -155,68 +172,69 @@ const Hackathon2023ProgramInfo = (): ReactElement => {
           container
           spacing={4}
           className={classes.gridContainer}
+          justifyContent="center"
         >
           {
             EVENTS.map(event => (
               <Grid
-              className={classes.gridItem}
-              item
-              key={event.title}
-              xs={12}
-              sm={6}
+                className={classes.gridItem}
+                item
+                key={event.title}
+                xs={12}
+                md={6}
               >
-                <div>
+                <div className={classes.containerRetroUI}>
                   <div className={classes.stepCircle}>
                     {event.step}
                   </div>
-                  <div className={classes.containerRetroUI}>
-                    <RetroUI
+                  <RetroUI
                     className={classes.retroUI}
                     title={(
                       <div className={classes.title}>
                         {event.title}
                       </div>
                     )}>
-                      <div className={classes.label}>
-                        {event.label}
-                      </div>
-                      <div className={classes.image}>
-                        {event.image}
-                      </div>
-                      <div className={classes.description}>
-                        {event.description}
-                      </div>
-                      <div className={classes.time}>
-                        {event.time}
-                      </div>
-                      <div className={classes.date}>
-                        {event.date}
-                      </div>
-                    </RetroUI>
-                  </div>
+                    <div className={classes.label}>
+                      {event.label}
+                    </div>
+                    <div className={classes.image}>
+                      <img src={event.image} />
+                    </div>
+                    <div className={classes.description}>
+                      {event.description}
+                    </div>
+                    <div className={classes.time}>
+                      {event.time}
+                    </div>
+                    <div className={classes.date}>
+                      {event.date}
+                    </div>
+                  </RetroUI>
                 </div>
               </Grid>
             ))
           }
         </Grid>
-        <Link href="/">
+        <div className={classes.containerCTAs}>
           <Button
-            className={classes.button}
             component="a"
             variant="raised"
             color="secondary"
+            href="#"
           >
             Pre-Register For 2023 Hackaton
           </Button>
-        </Link>
-        <div style={{ margin: '1rem'}}>
-          <Link href="/" className={classes.faq}>
-            Frequently Asked Questions
-          </Link>
+          <div>
+            <Button
+              component="a"
+              className={classes.buttonFaq}
+              href="#"
+            >
+              Frequently Asked Questions
+            </Button>
+          </div>
         </div>
-
       </div>
-
     </div>
   );
 };
