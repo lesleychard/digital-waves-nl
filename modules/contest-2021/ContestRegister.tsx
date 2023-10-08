1b5152bfd82f1ca3cf1459e100849f2ae23a7f4a
Lesley Chard <lac275@mun.ca>
2023-10-08 20:01:53 -0230

    Fixes small linting issues

--- modules/contest-2021/ContestRegister.tsx
+++ modules/contest-2021/ContestRegister.tsx
@@ -2,17 +2,6 @@ import { ReactElement } from 'react';
 import RegistrationForm from '../../components/RegistrationForm';
 
 const ContestRegister2021 = (): ReactElement => {
-  const title = (
-    <>
-      <span>
-        Register
-      </span>
-      &nbsp;for
-      <br />
-      Digital Waves 2021
-    </>
-  );
-
   return (
     <RegistrationForm />
   );
